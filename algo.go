8b7434e175fa12d36478d170f268c888ec0b05aa
eparisot <eparisot@student.42.fr>
2019-10-23 17:46:05 +0200

    set cube OK

--- algo.go
+++ algo.go
@@ -8,9 +8,10 @@ import (
 )
 
 type Env struct {
-	mix  []string
-	cube [6][3][3]int
-	res  string
+	mix        []string
+	cube       [6][3][3]int
+	solvedCube [6][3][3]int
+	res        string
 }
 
 func (env *Env) parseArgs(arg string) error {
@@ -34,19 +35,147 @@ func (env *Env) parseArgs(arg string) error {
 	return nil
 }
 
+func (env *Env) getSides(face int) [4]int {
+	var ret [4]int
+	if face == 0 {
+		ret = [4]int{3, 2, 4, 1}
+	} else if face == 5 {
+		ret = [4]int{3, 1, 4, 2}
+	} else if face == 1 {
+		ret = [4]int{0, 4, 5, 3}
+	} else if face == 2 {
+		ret = [4]int{0, 3, 5, 4}
+	} else if face == 3 {
+		ret = [4]int{5, 2, 0, 1}
+	} else if face == 4 {
+		ret = [4]int{5, 1, 0, 2}
+	}
+	return ret
+}
+
+func (env *Env) rotSides(cube *[6][3][3]int, face, way int) {
+	sides := env.getSides(face)
+	if way == 0 {
+		mem := cube[sides[0]][2][0]
+		cube[sides[0]][2][0] = cube[sides[1]][2][2]
+		cube[sides[1]][2][2] = cube[sides[2]][0][2]
+		cube[sides[2]][0][2] = cube[sides[3]][0][0]
+		cube[sides[3]][0][0] = mem
+		mem = cube[sides[0]][2][1]
+		cube[sides[0]][2][1] = cube[sides[1]][1][2]
+		cube[sides[1]][1][2] = cube[sides[2]][0][1]
+		cube[sides[2]][0][1] = cube[sides[3]][1][0]
+		cube[sides[3]][1][0] = mem
+		mem = cube[sides[0]][2][2]
+		cube[sides[0]][2][2] = cube[sides[1]][0][2]
+		cube[sides[1]][0][2] = cube[sides[2]][0][0]
+		cube[sides[2]][0][0] = cube[sides[3]][2][0]
+		cube[sides[3]][2][0] = mem
+	} else {
+		mem := cube[sides[0]][2][0]
+		cube[sides[0]][2][0] = cube[sides[3]][0][0]
+		cube[sides[3]][0][0] = cube[sides[2]][0][2]
+		cube[sides[2]][0][2] = cube[sides[1]][2][2]
+		cube[sides[1]][2][2] = mem
+		mem = cube[sides[0]][2][1]
+		cube[sides[0]][2][1] = cube[sides[3]][1][0]
+		cube[sides[3]][1][0] = cube[sides[2]][0][1]
+		cube[sides[2]][0][1] = cube[sides[1]][1][2]
+		cube[sides[1]][1][2] = mem
+		mem = cube[sides[0]][2][2]
+		cube[sides[0]][2][2] = cube[sides[3]][2][0]
+		cube[sides[3]][2][0] = cube[sides[2]][0][0]
+		cube[sides[2]][0][0] = cube[sides[1]][0][2]
+		cube[sides[1]][0][2] = mem
+	}
+}
+
+func (env *Env) rotate(face, way int) [6][3][3]int {
+	var cube [6][3][3]int
+	for i := range env.cube {
+		for j := range env.cube[i] {
+			for k := range env.cube[i][j] {
+				cube[i][j][k] = env.cube[i][j][k]
+			}
+		}
+	}
+	mem := cube[face][0][0]
+	if way == 0 {
+		cube[face][0][0] = cube[face][1][0]
+		cube[face][1][0] = cube[face][2][0]
+		cube[face][2][0] = cube[face][2][1]
+		cube[face][2][1] = cube[face][2][2]
+		cube[face][2][2] = cube[face][1][2]
+		cube[face][1][2] = cube[face][0][2]
+		cube[face][0][2] = cube[face][0][1]
+		cube[face][0][1] = mem
+	} else {
+		cube[face][0][0] = cube[face][0][1]
+		cube[face][0][1] = cube[face][0][2]
+		cube[face][0][2] = cube[face][1][2]
+		cube[face][1][2] = cube[face][2][2]
+		cube[face][2][2] = cube[face][2][1]
+		cube[face][2][1] = cube[face][2][0]
+		cube[face][2][0] = cube[face][1][0]
+		cube[face][1][0] = mem
+	}
+	env.rotSides(&cube, face, way)
+	return cube
+}
+
+func (env *Env) execStep(step string) {
+	stepID := 0
+	way := 0
+	nb := 1
+	if len(step) == 2 {
+		if string(step[1]) == "'" || string(step[1]) == "’" {
+			way = 1
+		} else if string(step[1]) == "2" {
+			nb = 2
+		}
+	} else if len(step) == 3 {
+		if string(step[1]) == "'" || string(step[1]) == "’" {
+			way = 1
+		}
+		if string(step[2]) == "2" {
+			nb = 2
+		}
+	}
+	if step[0] == 'F' {
+		stepID = 0
+	} else if step[0] == 'R' {
+		stepID = 1
+	} else if step[0] == 'U' {
+		stepID = 3
+	} else if step[0] == 'B' {
+		stepID = 5
+	} else if step[0] == 'L' {
+		stepID = 2
+	} else if step[0] == 'D' {
+		stepID = 4
+	}
+	env.cube = env.rotate(stepID, way)
+	if nb == 2 {
+		env.cube = env.rotate(stepID, way)
+	}
+	//fmt.Println(env.cube)
+}
+
 func (env *Env) shuffle() {
-	/*for step := range env.mix {
-		fmt.Println(env.mix[step])
-		TODO exec step
-	}*/
-	//fmt.Print(env.cube)
+	for step := range env.mix {
+		// exec step
+		env.execStep(env.mix[step])
+	}
 }
 
 func (env *Env) setCube() {
+	i := 0
 	for face := range env.cube {
 		for line := range env.cube[face] {
 			for col := range env.cube[face][line] {
-				env.cube[face][line][col] = face
+				env.cube[face][line][col] = i
+				env.solvedCube[face][line][col] = i
+				i++
 			}
 		}
 	}
@@ -68,5 +197,5 @@ func main() {
 		fmt.Println("Error : No args")
 	}
 	// TEST
-	fmt.Println("D D'2 D")
+	fmt.Println("F F")
 }
