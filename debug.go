dba41154cf4e1a5fd22fa86deaa9e2a070361e2c
rfautier <rfautier@student.42.fr>
2019-11-07 15:50:42 +0100

    add debugPrint

--- debug.go
+++ debug.go
@@ -1,7 +1,117 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func (env *Env) printcolor(str string) {
+	if str == "W" {
+		c := color.New(color.FgWhite)
+		c.Print("W")
+	}
+	if str == "R" {
+		c := color.New(color.FgRed)
+		c.Print("R")
+	}
+	if str == "O" {
+		c := color.New(color.FgMagenta)
+		c.Print("O")
+	}
+	if str == "B" {
+		c := color.New(color.FgBlue)
+		c.Print("B")
+	}
+	if str == "G" {
+		c := color.New(color.FgGreen)
+		c.Print("G")
+	}
+	if str == "Y" {
+		c := color.New(color.FgYellow)
+		c.Print("Y")
+	}
+}
+
+func (env *Env) printVerticalCube(tmpface [9]string) {
+	i := 0
+	for x := 0; x < 3; x++ {
+		for o := 0; o < 4; o++ {
+			fmt.Print(" ")
+		}
+		for o := 0; o < 3; o++ {
+			env.printcolor(tmpface[i])
+			i += 1
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func (env *Env) printMiddleCube(tmpStockface [3][9]string) {
+	for x := 0; x < 3; x++ {
+		for o := 0; o < 3; o++ {
+			env.printcolor(tmpStockface[x][o])
+		}
+		fmt.Print(" ")
+	}
+	fmt.Println()
+	for x := 0; x < 3; x++ {
+		for o := 3; o < 5; o++ {
+			if o == 4 {
+				if x == 0 {
+					env.printcolor("O")
+				}
+				if x == 1 {
+					env.printcolor("W")
+				}
+				if x == 2 {
+					env.printcolor("R")
+				}
+			}
+			env.printcolor(tmpStockface[x][o])
+		}
+		fmt.Print(" ")
+	}
+	fmt.Println()
+	for x := 0; x < 3; x++ {
+		for o := 5; o < 8; o++ {
+			env.printcolor(tmpStockface[x][o])
+		}
+		fmt.Print(" ")
+	}
+	fmt.Println()
+	fmt.Println()
+}
 
 func (env *Env) debugPrint(step string, cube [6]int32) {
-	fmt.Println(cube)
+	str := "WROBGY"
+	order := [6]int{5, 3, 2, 0, 1, 4}
+	var tmpStockface [3][9]string
+	x := 0
+
+	for _, j := range order {
+		face := cube[j]
+		var tmpface [9]string
+		for i := 8; i >= 0; i-- {
+			if i == 4 {
+				tmpface[4] = string(str[j])
+				continue
+			}
+			var nbr int32
+			nbr = face & 15
+			tmpface[i] = string(str[nbr])
+			face = face >> 4
+		}
+		if j == 5 || j == 3 || j == 4 {
+			env.printVerticalCube(tmpface)
+		}
+		if j == 2 || j == 0 || j == 1 {
+			tmpStockface[x] = tmpface
+			x += 1
+		}
+		if j == 1 {
+			env.printMiddleCube(tmpStockface)
+		}
+	}
 }
--- utils.go
+++ utils.go
@@ -1,5 +1,6 @@
 package main
 
+
 func (env *Env) copyCube(cube [6]int32) [6]int32 {
 	var copyCube [6]int32
 	for face := range cube {
@@ -25,13 +26,14 @@ func existInClosedList(currCube CubeEnv, closedList []CubeEnv) bool {
 }
 
 func (env *Env) setCube() {
-	env.currentCube.cube[0] = 0b0000000000000000
-	env.currentCube.cube[1] = 0b0001000100010001
-	env.currentCube.cube[2] = 0b0010001000100010
-	env.currentCube.cube[3] = 0b0011001100110011
-	env.currentCube.cube[4] = 0b0100010001000100
-	env.currentCube.cube[5] = 0b0101010101010101
+	env.currentCube.cube[0] = 0b00000000000000000000000000000000 // White
+	env.currentCube.cube[1] = 0b00010001000100010001000100010001 // Red
+	env.currentCube.cube[2] = 0b00100010001000100010001000100010 // Orange
+	env.currentCube.cube[3] = 0b00110011001100110011001100110011 // Blue
+	env.currentCube.cube[4] = 0b01000100010001000100010001000100 // Green
+	env.currentCube.cube[5] = 0b01010101010101010101010101010101 // Yellow (appear Magenta)
 	env.solvedCube = env.copyCube(env.currentCube.cube)
+	env.debugPrint("ok", env.currentCube.cube)
 }
 
 func (env *Env) shuffle() {
