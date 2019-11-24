7521ebfa30cd6e02685583cf09ef93426e371537
Romain Fautier <rfautier@student.42.fr>
2019-11-24 18:32:05 +0100

    deleted useless files

--- positiontopcorner.go
+++ positiontopcorner.go
@@ -102,3 +102,21 @@ func (env *Env) positiontopcorner() {
 		env.execFace("U R U' L' U R' U' L", ORANGE)
 	}
 }
+
+func (env *Env) orientedlastcorner() {
+	facetop := [4]int32{ORANGE, GREEN, RED, BLUE}
+	righttop := [4]int32{GREEN, RED, BLUE, ORANGE}
+	for i := 0; i < 4; i++ {
+		for true {
+			corner := ((env.currentCube.cube[ORANGE]>>20)&15) == facetop[i] &&
+				((env.currentCube.cube[GREEN]>>28)&15) == righttop[i] &&
+				((env.currentCube.cube[WHITE]>>12)&15) == WHITE
+			if corner {
+				break
+			}
+			env.execFace("R' D' R D", ORANGE)
+		}
+		env.execFace("U", ORANGE)
+	}
+	return
+}
--- topedges.go
+++ topedges.go
@@ -72,3 +72,35 @@ func (env *Env) topedges() {
 		}
 	}
 }
+
+
+
+func (env *Env) topcrossisFinnished(cube [6]int32) bool {
+	if ((cube[WHITE]>>8)&15) == WHITE &&
+		((cube[WHITE]>>16)&15) == WHITE &&
+		((cube[WHITE]>>24)&15) == WHITE &&
+		((cube[WHITE]>>0)&15) == WHITE {
+		return true
+	}
+	return false
+}
+
+func (env *Env) topcross() {
+	for true {
+		if ((env.currentCube.cube[ORANGE] >> 24) & 15) == WHITE {
+			env.execFace("F R U R' U' F'", ORANGE)
+		}
+		if ((env.currentCube.cube[GREEN] >> 24) & 15) == WHITE {
+			env.execFace("F R U R' U' F'", GREEN)
+		}
+		if ((env.currentCube.cube[RED] >> 8) & 15) == WHITE {
+			env.execFace("F R U R' U' F'", RED)
+		}
+		if ((env.currentCube.cube[BLUE] >> 24) & 15) == WHITE {
+			env.execFace("F R U R' U' F'", BLUE)
+		}
+		if env.topcrossisFinnished(env.currentCube.cube) {
+			return
+		}
+	}
+}
